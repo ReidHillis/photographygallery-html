a2ca5c66f80c02d579a3ab536f32a9724891aa78
DragonAlley64 <Jonathan.Leaper@ontariotechu.net>
2024-11-17 13:36:21 -0500

    added likes to the db

--- backend/server.js
+++ backend/server.js
@@ -13,16 +13,17 @@ db.run(`CREATE TABLE IF NOT EXISTS photos (
     id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
     title TEXT UNIQUE NOT NULL,
     author TEXT NOT NULL,
+    likes INT NOT NULL,
     photoData TEXT NOT NULL)`);
 
 server.post("/uploadPhoto", (req, res) => {
   let data = req.body;
 
   let statement = db.prepare(
-    `INSERT INTO photos(title, author, photoData) VALUES(?, ?, ?)`
+    `INSERT INTO photos(title, author, likes, photoData) VALUES(?, ?, ?, ?)`
   );
 
-  statement.run(data.title, data.author, data.photoData, (err) => {
+  statement.run(data.title, data.author, 0, data.photoData, (err) => {
     if (err) {
       console.error(err);
       return res.status(500).send("Error uploading photo");
@@ -45,6 +46,21 @@ server.get("/getPhoto", (req, res) => {
       return res.status(200).send(row);
     } else {
       return res.status(404).send("Photo not found");
+    }
+  });
+});
+
+server.get("addLike", (req, res) => {
+  let data = req.query;
+  let statement = db.prepare(
+    `UPDATE photos SET likes = likes + 1 WHERE id = ?`
+  );
+  db.run(statement, [data.id], (err) => {
+    if (err) {
+      console.error(err);
+      return res.sendStatus(500).send("Error updating likes");
+    } else {
+      return res.sendStatus(204).send("Likes updated");
     }
   });
 });
